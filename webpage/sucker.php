dfbb7953335fd52639c5769a75c8ecb65b85067e
khamidjon <egamov1998@mail.ru>
2018-03-07 16:13:48 +0500

    Second Change

---
+++ webpage/sucker.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Buy Your Way to a Better Education!</title>
+		<link href="buyagrade.css" type="text/css" rel="stylesheet" />
+	</head>
+<body>
+
+
+<h1>Thanks, sucker!</h1><br/>
+<p>Information has been recorded!</p>
+<?php
+	$name = $_GET["name"];
+	$section = $_GET["section_num"];
+	$credit_card_num = $_GET["cc_num"];
+	$cCard = $_GET["cc"];
+
+?>
+
+<dl>
+	<dt>Name</dt>
+	<dd><?php echo $name ?></dd>
+
+	<dt>Section</dt>
+	<dd><?php echo $section ?></dd>
+
+	<dt>Credit Card</dt>
+	<dd><?php echo $credit_card_num ?> (<?php echo $cCard ?>)</dd>
+</dl>
+
+
+</body>
+</html>
